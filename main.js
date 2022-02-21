d10055e0ec0585e12105f3e4cd3615739a37527e
Erick Esqueda <eriesqu19@gmail.com>
2022-02-20 21:48:57 -0800

    added proper password requirement validation
    at least 1 upper,lower,number and at least 8 characters, interactive if valid/invalid

--- main.js
+++ main.js
@@ -22,5 +22,43 @@ function validatePassword() {
   }
 }
 
+const pr1 = document.querySelector("#pr1");
+const pr2 = document.querySelector("#pr2");
+const pr3 = document.querySelector("#pr3");
+const pr4 = document.querySelector("#pr4");
+
+function passwordRequirements() {
+  const regexUpper = /[A-Z]/g;
+  const regexLower = /[a-z]/g;
+  const regexNumber = /[0-9]/g;
+
+  let pwd = password.value;
+
+  if (regexUpper.test(pwd)) {
+    pr1.textContent = "✓";
+  } else {
+    pr1.textContent = "";
+  }
+
+  if (regexLower.test(pwd)) {
+    pr2.textContent = "✓";
+  } else {
+    pr2.textContent = "";
+  }
+
+  if (regexNumber.test(pwd)) {
+    pr3.textContent = "✓";
+  } else {
+    pr3.textContent = "";
+  }
+
+  if (pwd.length >= 8) {
+    pr4.textContent = "✓";
+  } else {
+    pr4.textContent = "";
+  }
+}
+
 password.onchange = validatePassword;
+password.oninput = passwordRequirements;
 confirm_password.onkeyup = validatePassword;
