a5fc618c44e4897a6ac072ac11e5f3b4cd329c92
Erick Esqueda <eriesqu19@gmail.com>
2022-02-20 21:14:21 -0800

    added password validation

--- main.js
+++ main.js
@@ -10,3 +10,17 @@ input.forEach((item) => {
     element.classList.toggle("focus");
   });
 });
+
+const password = document.querySelector("#pwd");
+const confirm_password = document.querySelector("#confirm-pwd");
+
+function validatePassword() {
+  if (password.value !== confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity("");
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
